d81cbc3439cd8e34e521ddc5b5037499ad7cdf02
cjenk29 <48561804+cjenk29@users.noreply.github.com>
2019-03-28 10:55:54 -0500

    Create webmap2.js

---
+++ map2/webmap2.js
@@ -0,0 +1,20 @@
+let myMap = L.map('map1').setView([30.36, -91.06], 4)
+//L.tileLayer('https://cartodb-basemaps-{s}.global.ssl.fastly.net/dark_nolabels/{z}/{x}/{y}.png').addTo(myMap)
+var OpenStreetMap_BlackAndWhite = L.tileLayer('https://tiles.wmflabs.org/bw-mapnik/{z}/{x}/{y}.png', {
+	maxZoom: 18,
+	attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+}).addTo(myMap);
+let BRPoint = L.marker([30.36, -91.06]).addTo(myMap)
+let myPolygon = L.polygon([
+  [30, -90],
+  [31, -91],
+  [32, -90],
+]).addTo(myMap);
+let TigerStadium = L.polygon([
+	[30.4120, -91.1838],
+	[30.2385, -90.9201]
+]).addTo(myMap);
+
+myPolygon.bindPopup('New Orleans')
+BRPoint.bindPopup('A <strong>Baton Rouge</strong>')
+TigerStadium.bindPopup ('BR to Gonzales')
