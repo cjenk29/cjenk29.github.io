7f59a821dcb7fb1c65963acd79c44942fd3bbda8
cjenk29 <48561804+cjenk29@users.noreply.github.com>
2019-04-02 09:16:30 -0500

    Update webmap2.js

--- map2/webmap2.js
+++ map2/webmap2.js
@@ -6,9 +6,9 @@ let us = L.map('webmap2').setView([38, -100], 4.4);
   		transparent: true,
   		attribution: 'NOAA'
 	}).addTo(us)
-	L.tileLayer.wms('https://nowcoast.noaa.gov/arcgis/services/nowcoast/guidance_model_coastalocean_estofs_time/MapServer/WMSServer', {
- 		 layers: '1',
+	L.tileLayer.wms('https://mesonet.agron.iastate.edu/cgi-bin/wms/us/mrms_nn.cgi?', {
+ 		 layers: 'mrms_p72h',
  		 format: 'image/png',
   		transparent: true,
-  attribution: 'NOAA'
+  attribution: 'NOAA, Iowa State University'
 }).addTo(us);
