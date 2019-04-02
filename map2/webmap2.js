79c3e31c0e3d6bf236bedfcf3e469c112ccc76d8
cjenk29 <48561804+cjenk29@users.noreply.github.com>
2019-04-02 09:00:23 -0500

    Update webmap2.js

--- map2/webmap2.js
+++ map2/webmap2.js
@@ -1,14 +1,14 @@
-let Earth = L.map('webmap2').setView([40, -95], 5);
-	L.tileLayer('https://{s}.basemaps.cartocdn.com/dark_all/{z}/{x}/{y}{r}.png').addTo(Earth);
+let USA = L.map('webmap2').setView([38, -100], 4.4);
+	L.tileLayer('https://maps.heigit.org/openmapsurfer/tiles/roads/webmercator/{z}/{x}/{y}.png').addTo(USA);
 	L.tileLayer.wms('https://nowcoast.noaa.gov/arcgis/services/nowcoast/analysis_ocean_sfc_sst_time/MapServer/WMSServer', {
   		layers: '1',
   		format: 'image/png',
   		transparent: true,
   		attribution: 'NOAA'
-	}).addTo(Earth)
-	L.tileLayer.wms('https://nowcoast.noaa.gov/arcgis/services/nowcoast/sat_meteo_emulated_imagery_lightningstrikedensity_goes_time/MapServer/WMSServer', {
+	}).addTo(USA)
+	L.tileLayer.wms('https://nowcoast.noaa.gov/arcgis/services/nowcoast/guidance_model_coastalocean_estofs_time/MapServer/WMSServer', {
  		 layers: '1',
  		 format: 'image/png',
   		transparent: true,
   attribution: 'NOAA'
-}).addTo(Earth);
+}).addTo(USA);
