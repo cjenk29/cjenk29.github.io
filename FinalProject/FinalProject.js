594580e7515219d958387743d1623c342a1ea4cb
cjenk29 <48561804+cjenk29@users.noreply.github.com>
2019-05-02 15:05:44 -0500

    Update FinalProject.js

--- FinalProject/FinalProject.js
+++ FinalProject/FinalProject.js
@@ -1,28 +1,28 @@
-var slideIndex = 1;
-showSlides(slideIndex);
-
-// Next/previous controls
-function plusSlides(n) {
-  showSlides(slideIndex += n);
-}
-
-// Thumbnail image controls
-function currentSlide(n) {
-  showSlides(slideIndex = n);
-}
-
-function showSlides(n) {
-  var i;
-  var slides = document.getElementsByClassName("mySlides");
-  var dots = document.getElementsByClassName("dot");
-  if (n > slides.length) {slideIndex = 1}
-  if (n < 1) {slideIndex = slides.length}
-  for (i = 0; i < slides.length; i++) {
-      slides[i].style.display = "none";
-  }
-  for (i = 0; i < dots.length; i++) {
-      dots[i].className = dots[i].className.replace(" active", "");
-  }
-  slides[slideIndex-1].style.display = "block";
-  dots[slideIndex-1].className += " active";
-} 
+let usmap = L.map('webmap3').setView([37.5, -95], 4.0);
+let basemap = 'https://maps.heigit.org/openmapsurfer/tiles/roads/webmercator/{z}/{x}/{y}.png';
+L.tileLayer(basemap).addTo(usmap);
+let usmapDemographicsUrl = 'https://geog4046.github.io/portfolio/data/us_state_demographics_ESRI_2010A.geojson';
+jQuery.getJSON(usmapDemographicsUrl, function (data) {
+  let stateStyle = function (feature) {
+    let population = feature.properties.POPULATION;
+    let stateColor = 'purple';
+    if ( population >= '5000000'  ) { stateColor = 'teal' }
+    return {
+      stroke: false,
+      color: stateColor,
+      weight: 2,
+      fillOpacity: 0.5
+    };
+  };
+    let onEachFeature = function (feature, layer) {
+      let name = feature.properties.STATE_NAME;
+      let pop10 = feature.properties.POP2010;
+      let population = feature.properties.POPULATION;
+       layer.bindPopup('Total population of ' + name + ' ' + 'is' + ': ' + population + '<br>Population in 2010 was: ' + pop10 + ' ' )
+  };
+  let parksGeojsonOptions = {
+    style: stateStyle,
+    onEachFeature: onEachFeature
+  };
+  L.geoJSON(data, parksGeojsonOptions).addTo(usmap);
+})
